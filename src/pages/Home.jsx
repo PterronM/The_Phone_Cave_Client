4f67d668e03fe1ee16e90908c9a04f3cd945135d
Pilar Terron <pilarterronmartin@gmail.com>
2023-03-31 14:44:35 +0200

    Mejoras en estilos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
+
 function Home() {
   const [allMobile, setAllMobile] = useState(null);
   const [isFeaching, setIsFeaching] = useState(true);
@@ -18,7 +19,7 @@ function Home() {
       const response = await getAllMobile();
       setAllMobile(response.data);
       setIsFeaching(false);
-      console.log(response);
+      // console.log(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -31,10 +32,12 @@ function Home() {
       ) : (
         <div className="d-flex justify-content-between flex-wrap p-4">
           {allMobile.map((eachMobile) => {
+        
             return (
               <span key={eachMobile._id}>
                 <Card style={{ width: "18rem" }}>
                   <Card.Body>
+                    <Card.Img variant="top" src={`/img/${eachMobile.imageFileName}`} ></Card.Img>
                     <Card.Title>{eachMobile.name}</Card.Title>
                     <Card.Text>{eachMobile.manufacturer}</Card.Text>
                     <Link to={`/phones/${eachMobile._id}`}>
